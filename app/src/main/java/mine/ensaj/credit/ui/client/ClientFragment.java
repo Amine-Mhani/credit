88b40fca4077ee80587cba994b9a31385e0e928b
Amine-Mhani <aminemhani11@gmail.com>
2022-11-03 23:46:56 +0100

    update

---
+++ app/src/main/java/mine/ensaj/credit/ui/client/ClientFragment.java
@@ -0,0 +1,49 @@
+package mine.ensaj.credit.ui.client;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import mine.ensaj.credit.R;
+
+
+public class ClientFragment extends Fragment {
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private String mParam1;
+    private String mParam2;
+
+
+    public static ClientFragment newInstance(String param1, String param2) {
+        ClientFragment fragment = new ClientFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public ClientFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_client, container, false);
+    }
+}
